ea49bb144f990b8332eea4e08ee73f434af821b7
Ali <ali.moh.2000.ly@gmail.com>
2026-05-04 23:40:42 +0300

    Add workflow

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  server: {
-    open: true,
-    host: true,
-    port: 5173
-  },
+export default defineConfig(({ command, mode }) => {
+  const isProd = mode === "production";
+
+  return {
+    server: {
+      open: true,
+      host: true,
+      port: 5173,
+    },
+    base: isProd ? "/Frontend-Mentor-Weather-app/" : "/",
+  };
 });
